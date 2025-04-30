56e6a1360c7910964727c65a5089dba0976ad886
3276030701960004 <haniel.zefanya04@mastersystem.co.id>
2025-04-30 20:18:00 +0700

    add post method

--- odc-api-producer/src/main/java/id/co/jalin/odc_api_producer/controller/MessageController.java
+++ odc-api-producer/src/main/java/id/co/jalin/odc_api_producer/controller/MessageController.java
@@ -4,17 +4,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
-@RequestMapping("/api/message")
+// public class MessageController {
+//     @GetMapping("/ping")
+//     public String ping() {
+//         return "pong";
+//     }
+// }
+
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MessageController {
-
     private final KafkaTemplate<String, String> kafkaTemplate;
-    private static final String TOPIC = "dev_api";
+    private static final String DEV_API_TOPIC = "dev_api";
 
-    @PostMapping
+    @PostMapping("/messages")
     public String postNestedJson(@RequestBody String payload) {
-        kafkaTemplate.send(TOPIC, payload);
-        return "Successfully sent nested JSON to topic: " + TOPIC;
+        kafkaTemplate.send(DEV_API_TOPIC, payload);
+        return "Successfully sent nested JSON to topic: " + DEV_API_TOPIC;
     }
-}
+
+    // @PostMapping("/send")
+    // public String sendMessage(@RequestParam String topic, @RequestParam String message) {
+    //     kafkaTemplate.send(topic, message);
+    //     return "Message sent to topic " + topic;
+    // }
+}   
+
